4be208d117a0f191443bb380c51e5dc7fdc0e1cd
Natter Purson <58366270+npurson@users.noreply.github.com>
2020-05-14 19:19:22 +0800

    Modify indents

--- lab1-data/bits.c
+++ lab1-data/bits.c
@@ -25,68 +25,68 @@ editing the collection of functions in this source file.
 
 INTEGER CODING RULES:
 
-	Replace the "return" statement in each function with one
-	or more lines of C code that implements the function. Your code
-	must conform to the following style:
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code
+  must conform to the following style:
 
-	int Funct(arg1, arg2, ...) {
-			/* brief description of how your implementation works */
-			int var1 = Expr1;
-			...
-			int varM = ExprM;
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
 
-			varJ = ExprJ;
-			...
-			varN = ExprN;
-			return ExprR;
-	}
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
 
-	Each "Expr" is an expression using ONLY the following:
-	1. Integer constants 0 through 255 (0xFF), inclusive. You are
-			not allowed to use big constants such as 0xffffffff.
-	2. Function arguments and local variables (no global variables).
-	3. Unary integer operations ! ~
-	4. Binary integer operations & ^ | + << >>
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
 
-	Some of the problems restrict the set of allowed operators even further.
-	Each "Expr" may consist of multiple operators. You are not restricted to
-	one operator per line.
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
 
-	You are expressly forbidden to:
-	1. Use any control constructs such as if, do, while, for, switch, etc.
-	2. Define or use any macros.
-	3. Define any additional functions in this file.
-	4. Call any functions.
-	5. Use any other operations, such as &&, ||, -, or ?:
-	6. Use any form of casting.
-	7. Use any data type other than int.  This implies that you
-		 cannot use arrays, structs, or unions.
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
 
 
-	You may assume that your machine:
-	1. Uses 2s complement, 32-bit representations of integers.
-	2. Performs right shifts arithmetically.
-	3. Has unpredictable behavior when shifting an integer by more
-		 than the word size.
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
-	/*
-	 * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
-	 */
-	int pow2plus1(int x) {
-		 /* exploit ability of shifts to compute powers of 2 */
-		 return (1 << x) + 1;
-	}
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
 
-	/*
-	 * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
-	 */
-	int pow2plus4(int x) {
-		 /* exploit ability of shifts to compute powers of 2 */
-		 int result = (1 << x);
-		 result += 4;
-		 return result;
-	}
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
 
 FLOATING POINT CODING RULES
 
@@ -96,28 +96,28 @@ conditional control.  You are allowed to use both ints and unsigneds.
 You can use arbitrary integer and unsigned constants.
 
 You are expressly forbidden to:
-	1. Define or use any macros.
-	2. Define any additional functions in this file.
-	3. Call any functions.
-	4. Use any form of casting.
-	5. Use any data type other than int or unsigned.  This means that you
-		 cannot use arrays, structs, or unions.
-	6. Use any floating point data types, operations, or constants.
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
 
 
 NOTES:
-	1. Use the dlc (data lab checker) compiler (described in the handout) to
-		 check the legality of your solutions.
-	2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
-		 that you are allowed to use for your implementation of the function.
-		 The max operator count is checked by dlc. Note that '=' is not
-		 counted; you may use as many of these as you want without penalty.
-	3. Use the btest test harness to check your functions for correctness.
-	4. Use the BDD checker to formally verify your functions
-	5. The maximum number of ops for each function is given in the
-		 header comment for each function. If there are any inconsistencies
-		 between the maximum ops in the writeup and in this file, consider
-		 this file the authoritative source.
+  1. Use the dlc (data lab checker) compiler (described in the handout) to
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function.
+     The max operator count is checked by dlc. Note that '=' is not
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
 
 /*
  * STEP 2: Modify the following functions according the coding rules.
@@ -132,36 +132,36 @@ NOTES:
 
 #endif
 /* Copyright (C) 1991-2014 Free Software Foundation, Inc.
-	 This file is part of the GNU C Library.
+   This file is part of the GNU C Library.
 
-	 The GNU C Library is free software; you can redistribute it and/or
-	 modify it under the terms of the GNU Lesser General Public
-	 License as published by the Free Software Foundation; either
-	 version 2.1 of the License, or (at your option) any later version.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-	 The GNU C Library is distributed in the hope that it will be useful,
-	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	 Lesser General Public License for more details.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-	 You should have received a copy of the GNU Lesser General Public
-	 License along with the GNU C Library; if not, see
-	 <http://www.gnu.org/licenses/>.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
 /* This header is separate from features.h so that the compiler can
-	 include it implicitly at the start of every compilation.  It must
-	 not itself include <features.h> or any other header that includes
-	 <features.h> because the implicit include comes before any feature
-	 test macros that may be defined in a source file before it first
-	 explicitly includes a system header.  GCC knows the name of this
-	 header in order to preinclude it.  */
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
 /* glibc's intent is to support the IEC 559 math functionality, real
-	 and complex.  If the GCC (4.9 and later) predefined macros
-	 specifying compiler intent are available, use them to determine
-	 whether the overall intent is to support these features; otherwise,
-	 presume an older compiler has intent to support these features and
-	 define these macros by default.  */
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
 /* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
-	 Unicode 6.0.  */
+   Unicode 6.0.  */
 /* We do not support C11 <threads.h>.  */
 /*
  *   lsbZero - set 0 to the least significant bit of x
@@ -171,8 +171,8 @@ NOTES:
  *   Rating: 1
  */
 int lsbZero(int x) {
-	return x >> 1 << 1;
-	// return x & ~0xff;
+  return x >> 1 << 1;
+  // return x & ~0xff;
 }
 /*
  * byteNot - bit-inversion to byte n from word x
@@ -183,10 +183,10 @@ int lsbZero(int x) {
  *   Rating: 2
  */
 int byteNot(int x, int n) {
-	n = n << 3;
-	return x ^ 0xff << n;     // 与1异或相当于取反
-	// int nByte = x & 0xff << (n << 3);
-	// return x ^ nByte | nByte;
+  n = n << 3;
+  return x ^ 0xff << n;     // 与1异或相当于取反
+  // int nByte = x & 0xff << (n << 3);
+  // return x ^ nByte | nByte;
 }
 /*
  *   byteXor - compare the nth byte of x and y, if it is same, return 0, if not, return 1
@@ -199,8 +199,8 @@ int byteNot(int x, int n) {
  *   Rating: 2
  */
 int byteXor(int x, int y, int n) {
-	int mask = 0xff << (n << 3);
-	return !!(x & mask ^ y & mask);
+  int mask = 0xff << (n << 3);
+  return !!(x & mask ^ y & mask);
 }
 /*
  *   logicalAnd - x && y
@@ -209,7 +209,7 @@ int byteXor(int x, int y, int n) {
  *   Rating: 3
  */
 int logicalAnd(int x, int y) {
-	return !!x & !!y;
+  return !!x & !!y;
 }
 /*
  *   logicalOr - x || y
@@ -218,7 +218,7 @@ int logicalAnd(int x, int y) {
  *   Rating: 3
  */
 int logicalOr(int x, int y) {
-	return !!x | !!y;
+  return !!x | !!y;
 }
 /*
  * rotateLeft - Rotate x to the left by n
@@ -229,8 +229,8 @@ int logicalOr(int x, int y) {
  *   Rating: 3
  */
 int rotateLeft(int x, int n) {
-	int mask = 1 << 31 >> 31 + ~n + 1;
-	return x >> 32 + ~n + 1 & ~mask | x << n;
+  int mask = 1 << 31 >> 31 + ~n + 1;
+  return x >> 32 + ~n + 1 & ~mask | x << n;
 }
 /*
  * parityCheck - returns 1 if x contains an odd number of 1's
@@ -240,24 +240,24 @@ int rotateLeft(int x, int n) {
  *   Rating: 4
  */
 int parityCheck(int x) {
-	x = x ^ x << 16;          // 存储在高8位可以避免右移后需要清0的问题
-	x = x ^ x << 8;
-	x = x ^ x << 4;
-	x = x ^ x << 2;
-	x = x ^ x << 1;
-	return x >> 31 & 0x1;
+  x = x ^ x << 16;          // 存储在高8位可以避免右移后需要清0的问题
+  x = x ^ x << 8;
+  x = x ^ x << 4;
+  x = x ^ x << 2;
+  x = x ^ x << 1;
+  return x >> 31 & 0x1;
 
-	// int mask = 0xff << 8 + 0xff;
-	// x = x & mask ^ x >> 16 & mask;
-	// mask = mask >> 8;
-	// x = x & mask ^ x >> 8;
-	// mask = mask >> 4;
-	// x = x & mask ^ x >> 4;
-	// mask = mask >> 2;
-	// x = x & mask ^ x >> 2;
-	// mask = mask >> 1;
-	// x = x & mask ^ x >> 1;
-	// return x;
+  // int mask = 0xff << 8 + 0xff;
+  // x = x & mask ^ x >> 16 & mask;
+  // mask = mask >> 8;
+  // x = x & mask ^ x >> 8;
+  // mask = mask >> 4;
+  // x = x & mask ^ x >> 4;
+  // mask = mask >> 2;
+  // x = x & mask ^ x >> 2;
+  // mask = mask >> 1;
+  // x = x & mask ^ x >> 1;
+  // return x;
 }
 /*
  * mul2OK - Determine if can compute 2*x without overflow
@@ -269,7 +269,7 @@ int parityCheck(int x) {
  *   Rating: 2
  */
 int mul2OK(int x) {
-	return ~(x >> 31 & 0x1 ^ x >> 30 & 0x1) & 0x1;    // x第31位和第30位异或
+  return ~(x >> 31 & 0x1 ^ x >> 30 & 0x1) & 0x1;    // x第31位和第30位异或
 }
 /*
  * mult3div2 - multiplies by 3/2 rounding toward 0,
@@ -283,8 +283,8 @@ int mul2OK(int x) {
  *   Rating: 2
  */
 int mult3div2(int x) {
-	x = (x << 1) + x;
-	return (x >> 1) + (x >> 31 & x & 0x1);            // 向0方向取整
+  x = (x << 1) + x;
+  return (x >> 1) + (x >> 31 & x & 0x1);            // 向0方向取整
 }
 /*
  * subOK - Determine if can compute x-y without overflow
@@ -295,8 +295,8 @@ int mult3div2(int x) {
  *   Rating: 3
  */
 int subOK(int x, int y) {
-	int mask = 1 << 31;
-	return !((x & mask ^ y & mask) & (x & mask ^ (x + ~y + 1 & mask)));
+  int mask = 1 << 31;
+  return !((x & mask ^ y & mask) & (x & mask ^ (x + ~y + 1 & mask)));
 }
 /*
  * absVal - absolute value of x
@@ -307,8 +307,8 @@ int subOK(int x, int y) {
  *   Rating: 4
  */
 int absVal(int x) {
-	int m = x >> 31;
-	return (x ^ m) + ~m + 1;                  // 正数则不变，负数则^m相当于取反，-m相当于+1
+  int m = x >> 31;
+  return (x ^ m) + ~m + 1;                  // 正数则不变，负数则^m相当于取反，-m相当于+1
 }
 /*
  * float_abs - Return bit-level equivalent of absolute value of f for
@@ -322,9 +322,9 @@ int absVal(int x) {
  *   Rating: 2
  */
 unsigned float_abs(unsigned uf) {
-	if ((uf & 0x7fffffff) > 0x7f800000)       // NaN
-		return uf;
-	return uf & ~(1 << 31);
+  if ((uf & 0x7fffffff) > 0x7f800000)       // NaN
+    return uf;
+  return uf & ~(1 << 31);
 }
 /*
  * float_f2i - Return bit-level equivalent of expression (int) f
@@ -339,16 +339,16 @@ unsigned float_abs(unsigned uf) {
  *   Rating: 4
  */
 int float_f2i(unsigned uf) {
-	int s = (uf & 1 << 31) >> 31;             // sign
-	int e = ((uf & 0x7f800000) >> 23) - 127;  // exponent
-	int m = uf & ~(1 << 31 >> 8) | 1 << 23;   // mantissa
-	
-	if (e == 127) return 0x80000000;          // NaN or infinity
-	if (!(uf & 0x7fffffff)) return 0;         // exponent = mantissa = 0
-	
-	e = 23 - e;
-	if (e > 32) return 0;                     // 0.x
-	if (e < -8) return 0x80000000;            // overflow
-	if (e > 0) return (m >> e ^ s) + s & 0x1; // exponent < 23, right shift and calculate complement
-	return (m << -e ^ s) + s & 0x1;           // 23 < exponent < 31, left shift and calculate complement
+  int s = (uf & 1 << 31) >> 31;             // sign
+  int e = ((uf & 0x7f800000) >> 23) - 127;  // exponent
+  int m = uf & ~(1 << 31 >> 8) | 1 << 23;   // mantissa
+  
+  if (e == 127) return 0x80000000;          // NaN or infinity
+  if (!(uf & 0x7fffffff)) return 0;         // exponent = mantissa = 0
+  
+  e = 23 - e;
+  if (e > 32) return 0;                     // 0.x
+  if (e < -8) return 0x80000000;            // overflow
+  if (e > 0) return (m >> e ^ s) + s & 0x1; // exponent < 23, right shift and calculate complement
+  return (m << -e ^ s) + s & 0x1;           // 23 < exponent < 31, left shift and calculate complement
 }
